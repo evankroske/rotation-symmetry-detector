b1e71b463aeda8cca42440ff7a472da92584a174
Evan Kroske <evan@evankroske.com>
2012-07-16 17:41:54 -0600

    Improved performance on detecting artificial circles but not detecting continuous rotational symmetry in pictures.

--- sym_rss_point.m
+++ sym_rss_point.m
@@ -1,38 +1,46 @@
 function rss_value = sym_rss_point (im, x, y, n)
 % Compute RSS value for a given point
 
+	half = ceil(n / 2);
+
 	function esd = energy_spectral_density (dft_coeffs)
 		esd = cellfun(@(row) (row .* conj(row)), dft_coeffs, ...
 			'UniformOutput', false);
 	end
 
 	function output = dominant_frequencies (esd)
-		half = ceil(n / 2);
+		mask = logical(ones(1, half));
+		mask(1:2) = false;
 		output = cellfun( ...
 			@(row) row > ...
-				mean(row(2:half)) + ...
-				2 * std(row(2:half)), ...
+				mean(row(3:half)) + ...
+				2 * std(row(3:half)) & ...
+				mask, ...
 			esd, ...
 			'UniformOutput', false);
 	end
 
 	function x = row_rss_value (k_peaks_r, esd_r)
-		if any(esd_r) && any(k_peaks_r)
-			a = 0:n - 1;
+		if any(k_peaks_r)
+			a = 0:half - 1;
 			peak_indices = a(k_peaks_r);
-			min_peak_index = min(peak_indices(peak_indices > 0));
-			if ~isempty(min_peak_index) && min_peak_index == 1
-				rho_r = 0;
-			elseif ~isempty(min_peak_index)
-				rho_r = ~any( ...
-					mod(peak_indices, min_peak_index));
+			min_peak_index = min(peak_indices);
+			rho_r = ~any( ...
+				mod(peak_indices, min_peak_index));
+			x = rho_r * mean(esd_r(k_peaks_r)) / mean(esd_r(3:half));
+		elseif ~any(k_peaks_r)
+			if esd_r(2) > 0
+				x = esd_r(1) / esd_r(2);
 			else
-				rho_r = 1;
+				x = esd_r(1);
 			end
-			x = rho_r * mean(esd_r(k_peaks_r)) / mean(esd_r);
 		else
 			x = 0;
 		end
+	end
+	
+	function x = subvector (v, indices)
+		x = v(indices);
 	end
 
 	rss_value = 0;
@@ -40,7 +48,8 @@ function rss_value = sym_rss_point (im, x, y, n)
 	if length(fep) == 0
 		return;
 	end
-	dft_coeffs = cellfun(@(row) (fft(row)), fep, 'UniformOutput', false);
+	dft_coeffs = cellfun(@(row) subvector(fft(row), 1:half), fep, ...
+		'UniformOutput', false);
 	esd = energy_spectral_density(dft_coeffs);
 	k_peaks = dominant_frequencies(esd);
 	rss_value = sum(cell2mat( ...

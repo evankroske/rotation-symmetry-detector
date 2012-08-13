3674fe360610e53da22e529b47aefdcb03efc02e
Evan Kroske <evan@evankroske.com>
2012-08-13 16:15:17 -0400

    Fixed sym_peaks_sub bug.

--- sym_peaks_sub.m
+++ sym_peaks_sub.m
@@ -1,10 +1,10 @@
-function [rows columns] = sym_peaks_sub (rss_map, beta)
+function [max_peaks_rows max_peaks_cols] = sym_peaks_sub (rss_map, beta)
 % Finds coordinates of local maxima greater than mean + beta * std
 
 	maxima_mask = rss_map > imdilate(rss_map, [1 1 1; 1 0 1; 1 1 1]);
 	threshold = rss_map(maxima_mask) >= mean(rss_map(:)) + ...
 		beta * std(rss_map(:));
-	[all_xs, all_ys] = find(maxima_mask);
-	xs = all_xs(threshold);
-	ys = all_ys(threshold);
+	[peaks_rows, peaks_cols] = find(maxima_mask);
+	max_peaks_rows = peaks_rows(threshold);
+	max_peaks_cols = peaks_cols(threshold);
 end
